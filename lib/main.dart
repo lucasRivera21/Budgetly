c25eb42aee26ba75dce83d57ed6cc9ed985b7936
lucas <lucasriverar2001@gmail.com>
2026-04-06 20:47:18 -0500

    Add styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.onestTextTheme(Theme.of(context).textTheme),
       ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
---
+++ lib/theme/custom_color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const surface = Color(0xFFF5F8F4);
+  static const onSurface = Color(0xFF0B1410);
+  static const primary = Color(0xFF0F8A5F);
+  static const onPrimary = Color(0xFFFFFFFF);
+  static const primaryContainer = Color(0xFFE7F5EE);
+  static const onPrimaryContainer = Color(0xFF0B5D42);
+  static const surfaceContainer = Color(0xFFFFFFFF);
+  static const onSurfaceContainer = Color(0xFF6C7A71);
+  static const outline = Color(0xFFDDE7E0);
+  static const errorContainer = Color(0xFFFEE2E2);
+  static const onErrorContainer = Color(0xFFB42318);
+}
